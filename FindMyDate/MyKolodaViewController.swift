011998f9a8f7bace45c76dc1806b398d2189a251
Chris Dean <chrisdean@node-1w7jr9qk65qxqb090pm01hm4v.ipv6.telus.net>
2017-08-17 15:06:41 -0700

    Got multi-thread going for swipe function

--- FindMyDate/MyKolodaViewController.swift
+++ FindMyDate/MyKolodaViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Koloda
 import Firebase
+import pop
+
+private var numberOfCards: Int = 5
 
 class MyKolodaViewController: UIViewController {
 
@@ -16,33 +19,59 @@ class MyKolodaViewController: UIViewController {
     
     let storageRef = Storage.storage().reference(forURL: "gs://findmydate-1c6f4.appspot.com/")
     let usersRef = Database.database().reference(withPath: "users")
-    var users : [FirebaseUser] = []
-    var images : [UIImage] = []
-
+    var users = Array<FirebaseUser>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         kolodaView.dataSource = self
-        kolodaView.delegate = self as! KolodaViewDelegate
-        // Do any additional setup after loading the view.
-        
-        usersRef.observe(.childAdded, with: { snap in
-            let snapValue = snap.value as? NSDictionary
-            
-            guard let getDisplayName = snapValue?["name"] as? String else {return}
-            //self.currentUsers.append(getDisplayName)
-            
-            guard let getProfileURL = snapValue?["profileURL"] as? String else {return}
-            let url = NSURL(string: getProfileURL)! as URL
-            guard let getEmail = snapValue?["email"] as? String else {return}
-            guard let getUid = snapValue?["uid"] as? String else {return}
-            //let profilePicRef = self.storageRef.child(getUid + "/profile_pic.jpg")
-            
-            let user = FirebaseUser(uid: getUid, email: getEmail, name: getDisplayName, profileURL: url)
-            self.users.append(user)
-        })
+        kolodaView.delegate = self// as? KolodaViewDelegate
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        //fetchPhotos(completion: <#([FirebaseUser]) -> ()#>)
+//        mapRegion { (region) in
+//            
+//            if let region = region {
+//                
+//                self.mapView.setRegion(region, animated: true)
+//            }
+//        }
+        //DispatchQueue.main.async(execute: { () -> Void in
+        //DispatchQueue.main.async(execute: { () -> Void in
+            fetchPhotos() {
+                (_: [FirebaseUser]) in
+                print("fetched urls")
+            }
+        //})
     }
+    
 
+    
+    func fetchPhotos(completion: @escaping (Array<FirebaseUser>) -> ()) {
+        //DispatchQueue.main.async {
+        //var users: Array<FirebaseUser> = []
+        self.usersRef.observe(.value, with: {snap in
+            var users = Array<FirebaseUser>()
+            
+            if let snapshots = snap.children.allObjects as? [DataSnapshot] {
+                for snapShot in snapshots {
+                    guard let getName = snapShot.childSnapshot(forPath: "name").value! as? String else {return}
+                    guard let getProfileURL = snapShot.childSnapshot(forPath: "profileURL").value! as? String else {return}
+                    let url = NSURL(string: getProfileURL)! as URL
+                    guard let getEmail = snapShot.childSnapshot(forPath: "email").value! as? String else {return}
+                    guard let getUid = snapShot.childSnapshot(forPath: "uid").value! as? String else {return}
+                    let user = FirebaseUser(uid: getUid, email: getEmail, name: getName, profileURL: url)
+                    users.append(user)
+                    
+                    //print(users.count)
+                }
+            }
+            self.users = users
+            self.kolodaView.reloadData()
+            completion(users)
+        })
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -62,10 +91,19 @@ class MyKolodaViewController: UIViewController {
 
 }
 
+extension MyKolodaViewController: KolodaViewDelegate {
+    
+    func kolodaDidSwipedCardAtIndex(_ koloda: KolodaView, index: UInt, direction: SwipeResultDirection) {
+
+        //self.kolodaView.reloadData()
+    }
+}
+
 extension MyKolodaViewController: KolodaViewDataSource {
     
     func kolodaNumberOfCards(_ koloda:KolodaView) -> Int {
-        return users.count
+        print(self.users.count)
+        return self.users.count
     }
     
     func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
@@ -73,20 +111,14 @@ extension MyKolodaViewController: KolodaViewDataSource {
     }
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
-        
+        let photoView = Bundle.main.loadNibNamed("KolodaPhotoView", owner: self, options: nil)?[0] as? KolodaPhotoView
+        //var photoView: KolodaPhotoView?
         let user = users[Int(index)]
-        var profileImage: UIImage!
-        
-        let profilePicRef = self.storageRef.child(user.uid + "/profile_pic.jpg")
-        profilePicRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
-            if error != nil {
-                print("an error occurred when downloading profile picture from firebase storage")
-            } else {
-                profileImage = UIImage(data: data!)!
-            }
-        }
-   
-        return UIImageView(image: profileImage)
+        photoView?.photoImage?.imageFromURL(user.profileURL.absoluteString)
+        photoView?.photoTitle?.text = user.name
+        print(user.name)
+        print(user.profileURL.absoluteString) 
+        return photoView!
     }
     
     func koloda(koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
