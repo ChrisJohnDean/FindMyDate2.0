5adf4a524a949c92b8988af1e36443c83a7a61fb
Chris Dean <chrisdean@node-1w7jr9qk65qxocgerpde48dzz.ipv6.telus.net>
2017-09-21 01:25:59 -0700

    Testing if I can commit to Github after having just created a Github pages

--- FindMyDate/YesOrNoViewController.swift
+++ FindMyDate/YesOrNoViewController.swift
@@ -55,7 +55,6 @@ class YesOrNoViewController: UIViewController {
         })
     }
     /*
-    // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
