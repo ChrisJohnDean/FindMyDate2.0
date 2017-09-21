c29eb19b9ff98f79812f26fbaa1b2ee61e08b6e4
Chris Dean <chrisdean@node-1w7jr9qk65qxocgerpde48dzz.ipv6.telus.net>
2017-09-21 01:19:42 -0700

    Testing if I can commit to Github after having just created a Github pages

--- FindMyDate/YesOrNoViewController.swift
+++ FindMyDate/YesOrNoViewController.swift
@@ -40,7 +40,7 @@ class YesOrNoViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
     func deleteDate() {
